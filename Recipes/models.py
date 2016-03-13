1e326787c64f1842582fe2e26a40e293cb80dfa6
Adam Collins <adc82@case.edu>
2016-03-13 01:53:29 -0500

    Test for Recipes written

--- Recipes/models.py
+++ Recipes/models.py
@@ -11,24 +11,29 @@ class Recipe(models.Model):
     description = models.TextField()
 
     def insert_action(self, action, position):
+        if action.recipe != self and action.recipe != None:
+            return None
         from Recipes.models import GeneralAction
-        if position < 0:
+        if position <= 0:
             position = self.actions.count() + 1
 
         number_of_actions = self.actions.count()
-        i = number_of_actions
 
         if position < 0 or position > number_of_actions:
             position = number_of_actions + 1
 
+        i = number_of_actions
         while i >= position:
-            action = GeneralAction.objects.get(recipe=self,
-                                               instruction_number=i)
-            action.instruction_number = action.instruction_number + 1
+            temp_action = GeneralAction.objects.get(recipe=self,
+                                                    instruction_number=i)
+            temp_action.instruction_number = temp_action.instruction_number + 1
+            temp_action.save()
 
             i = i - 1
 
         action.instruction_number = position
+        action.recipe = self
+        action.save()
 
         return action
 
@@ -47,22 +52,20 @@ class BasicReturnText(models.Model):
 
 
 class GeneralAction(models.Model):
-    recipe = models.ForeignKey(Recipe, related_name='actions')
+    recipe = models.ForeignKey(Recipe, null=True, related_name='actions')
 
     choices = (
         ('RT', 'Basic Return Text'),
         )
 
-    instruction_number = models.IntegerField(unique=True)
+    instruction_number = models.IntegerField(default=-1)
     type = models.CharField(max_length=2, choices=choices)
 
     basic_return_text = models.OneToOneField(BasicReturnText, null=True)
 
     def get_action(self):
-        if self.choice == 'RT':
+        if self.type == 'RT':
             action = self.basic_return_text
         instruction = self.instruction_number
-        next_instruction, return_statement = action.action(instruction)
-        self.instruction_number = next_instruction
+        return action.action(instruction)
 
-        return return_statement
--- Recipes/tests.py
+++ Recipes/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.test.client import Client
 
 from accounts.models import User
-from .models import Recipe, GeneralAction
+from .models import Recipe, GeneralAction, BasicReturnText
 
 
 class RecipesViewsTestCases(TestCase):
@@ -75,16 +75,137 @@ class RecipesViewsTestCases(TestCase):
 class RecipesModelsTestCases(TestCase):
 
     def setUp(self):
-        pass
+        self.password = '$tr0ngPa$$w0rd'
+
+        self.user = User.objects.create_user(
+            first_name='Adam',
+            last_name='Collins',
+            email='adc82@case.edu',
+            password=self.password
+            )
+
+        self.recipe = Recipe.objects.create(
+            creator=self.user,
+            name='First App',
+            description='First Description'
+            )
+
+        self.basic_return_text = BasicReturnText.objects.create(
+            return_statement='Hello World!'
+            )
+
+        self.general_action = GeneralAction.objects.create(
+            type='RT',
+            basic_return_text=self.basic_return_text
+            )
 
     def test_recipe_insert_action(self):
-        pass
+        number_of_actions = self.recipe.actions.count()
+        self.assertEqual(number_of_actions, 0)
 
-    def test_recipe_add_action(self):
-        pass
+        self.recipe.insert_action(self.general_action, 0)
+
+        self.general_action = GeneralAction.objects.get(
+            pk=self.general_action.pk)
+
+        number_of_actions = self.recipe.actions.count()
+        self.assertEqual(number_of_actions, 1)
+
+        basic_return_text2 = BasicReturnText.objects.create(
+            return_statement='Hello World!2'
+            )
+        general_action2 = GeneralAction.objects.create(
+            type='RT',
+            basic_return_text=basic_return_text2
+            )
+
+        self.recipe.insert_action(general_action2, 10)
+
+        self.general_action = GeneralAction.objects.get(
+            pk=self.general_action.pk)
+        general_action2 = GeneralAction.objects.get(
+            pk=general_action2.pk)
+        number_of_actions = self.recipe.actions.count()
+        self.assertEqual(number_of_actions, 2)
+        self.assertEqual(self.general_action.instruction_number, 1)
+        self.assertEqual(general_action2.instruction_number, 2)
+
+        basic_return_text3 = BasicReturnText.objects.create(
+            return_statement='Hello World!3'
+            )
+        general_action3 = GeneralAction.objects.create(
+            type='RT',
+            basic_return_text=basic_return_text3
+            )
+        self.recipe.insert_action(general_action3, -10)
+        self.general_action = GeneralAction.objects.get(
+            pk=self.general_action.pk)
+        general_action2 = GeneralAction.objects.get(
+            pk=general_action2.pk)
+        general_action3 = GeneralAction.objects.get(
+            pk=general_action3.pk)
+        number_of_actions = self.recipe.actions.count()
+        self.assertEqual(number_of_actions, 3)
+        self.assertEqual(self.general_action.instruction_number, 1)
+        self.assertEqual(general_action2.instruction_number, 2)
+        self.assertEqual(general_action3.instruction_number, 3)
+
+        basic_return_text4 = BasicReturnText.objects.create(
+            return_statement='Hello World!4'
+            )
+        general_action4 = GeneralAction.objects.create(
+            type='RT',
+            basic_return_text=basic_return_text4
+            )
+        self.recipe.insert_action(general_action4, 2)
+        self.general_action = GeneralAction.objects.get(
+            pk=self.general_action.pk)
+        general_action2 = GeneralAction.objects.get(
+            pk=general_action2.pk)
+        general_action3 = GeneralAction.objects.get(
+            pk=general_action3.pk)
+        general_action4 = GeneralAction.objects.get(
+            pk=general_action4.pk)
+        number_of_actions = self.recipe.actions.count()
+        self.assertEqual(number_of_actions, 4)
+        self.assertEqual(self.general_action.instruction_number, 1)
+        self.assertEqual(general_action4.instruction_number, 2)
+        self.assertEqual(general_action2.instruction_number, 3)
+        self.assertEqual(general_action3.instruction_number, 4)
+
+        basic_return_text5 = BasicReturnText.objects.create(
+            return_statement='Hello World!5'
+            )
+        general_action5 = GeneralAction.objects.create(
+            type='RT',
+            basic_return_text=basic_return_text5
+            )
+        self.recipe.insert_action(general_action5, 100)
+        self.general_action = GeneralAction.objects.get(
+            pk=self.general_action.pk)
+        general_action2 = GeneralAction.objects.get(
+            pk=general_action2.pk)
+        general_action3 = GeneralAction.objects.get(
+            pk=general_action3.pk)
+        general_action4 = GeneralAction.objects.get(
+            pk=general_action4.pk)
+        general_action5 = GeneralAction.objects.get(
+            pk=general_action5.pk)
+        number_of_actions = self.recipe.actions.count()
+        self.assertEqual(number_of_actions, 5)
+        self.assertEqual(self.general_action.instruction_number, 1)
+        self.assertEqual(general_action4.instruction_number, 2)
+        self.assertEqual(general_action2.instruction_number, 3)
+        self.assertEqual(general_action3.instruction_number, 4)
+        self.assertEqual(general_action5.instruction_number, 5)
 
     def test_basic_return_text_action(self):
-        pass
+        next_instruction, return_text = self.basic_return_text.action(1)
+        self.assertEqual(next_instruction, 2)
+        self.assertEqual(return_text, self.basic_return_text.return_statement)
 
     def test_general_action_get_action(self):
-        pass
+        next_instruction, return_text = self.general_action.get_action()
+        self.assertEqual(next_instruction,
+                         self.general_action.instruction_number + 1)
+        self.assertEqual(return_text, self.basic_return_text.return_statement)
