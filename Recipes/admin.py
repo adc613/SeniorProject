7eea9268bd1fcda1ddec2289d3f72e2fb873a966
Ubuntu <ubuntu@ip-172-31-58-166.ec2.internal>
2016-03-20 21:52:39 +0000

    bad commit message

--- Recipes/admin.py
+++ Recipes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(Recipe)
+admin.site.register(BasicReturnText)
+admin.site.register(GeneralAction)
