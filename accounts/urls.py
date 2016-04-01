e819da56d89745bc8945d4c0013be61971d627f5
Adam Collins <adc82@case.edu>
2016-04-01 13:51:07 -0400

    vim fugitive isn't working the way I thought it would ignore this message

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,8 @@ import views
 urlpatterns = [
     url(r'^createaccount/$', views.CreateAccountView.as_view(),
         name='create_account'),
+    url(r'^dashboard/$', views.DashboardView.as_view(),
+        name='dashboard'),
     url(r'^login/$', views.LoginView.as_view(), name='login'),
     url(r'^logout/$', views.logout_view, name='logout'),
     url(r'^linktoecho/$', views.LinkEchoToUserView.as_view(),
